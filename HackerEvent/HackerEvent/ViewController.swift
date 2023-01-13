fe375e5fa36d260db34407f681971c39a0eb31b7
Khalil Mhelheli <kmhelheli@mobelite.fr>
2023-01-13 15:57:15 +0100

    first commit

---
+++ HackerEvent/HackerEvent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackerEvent
+//
+//  Created by Khalil Mhelheli on 13/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
