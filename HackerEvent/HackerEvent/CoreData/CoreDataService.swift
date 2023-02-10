84ec9e8fc8e40df51a18d3cf1083aa58145af127
Khalil Mhelheli <kmhelheli@mobelite.fr>
2023-02-10 12:04:13 +0100

    create core data model with service

--- HackerEvent/HackerEvent/CoreData/CoreDataService.swift
+++ HackerEvent/HackerEvent/CoreData/CoreDataService.swift
@@ -6,3 +6,84 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataService: DBServiceProtocol {
+    
+    static let shared = CoreDataService()
+    
+    typealias ObjectType = NSManagedObject
+    typealias PredicateType = NSPredicate
+    
+    // MARK: - Core Data
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+        let container = NSPersistentCloudKitContainer(name: "EventModelDB")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    var context: NSManagedObjectContext { persistentContainer.viewContext }
+    
+    func create(_ object: NSManagedObject) {
+        do {
+            try context.save()
+        } catch {
+            fatalError("error saving context while creating an object")
+        }
+    }
+    
+    func retrieve<T: NSManagedObject>(_ objectType: T.Type, predicate: NSPredicate? = nil, limit: Int? = nil) -> Result<[T], Error> {
+        let request = objectType.fetchRequest()
+        request.predicate = predicate
+        if let limit = limit {
+            request.fetchLimit = limit
+        }
+        do {
+            let result = try context.fetch(request)
+            return .success(result as? [T] ?? [])
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    
+    func retrieveFirst<T: NSManagedObject>(_ objectType: T.Type, predicate: NSPredicate?) -> Result<T?, Error> {
+        let request = objectType.fetchRequest()
+        request.predicate = predicate
+        request.fetchLimit = 1
+        do {
+            let result = try context.fetch(request) as? [T]
+            return .success(result?.first)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    func update(_ object: NSManagedObject) {
+        do {
+            try context.save()
+        } catch {
+            fatalError("error saving context while updating an object")
+        }
+    }
+    
+    func delete(_ object: NSManagedObject) {
+        context.delete(object)
+    }
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
--- HackerEvent/HackerEvent/DataBase/DataBaseManager.swift
+++ HackerEvent/HackerEvent/DataBase/DataBaseManager.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+protocol DataManagerProtocol {
+    func retrieveEvents(includingCompleted: Bool) -> [Event]
+    func addEvent(name: String)
+}
+
+extension DataManagerProtocol {
+    func retrieveTodos(includingCompleted: Bool = false) -> [Event] {
+        retrieveTodos(includingCompleted: includingCompleted)
+    }
+}
+
+class DataManager {
+    static let shared: DataManagerProtocol = DataManager()
+    var dbService: CoreDataService = CoreDataService.shared
+}
+
+
+extension DataManager: DataManagerProtocol {
+    
+    func retrieveEvents(includingCompleted: Bool) -> [Event] {
+        let predicate = includingCompleted ? nil : NSPredicate(format: "isCompletd == false")
+        let result: Result<[EventEntity], Error> = dbService.retrieve(EventEntity.self, predicate: predicate)
+        switch result {
+        case .success(let eventEntities):
+            return eventEntities.map { $0.convertToEvent()}
+        case .failure(let error):
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    func addEvent(name: String) {
+        let entity = EventEntity.entity()
+        let newEvent = EventEntity(entity: entity, insertInto: dbService.context)
+        newEvent.name = name
+        dbService.create(newEvent)
+    }
+ 
+}
